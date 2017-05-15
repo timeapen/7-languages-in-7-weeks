c23a375a654460fa7b3507678fb65dc6279bf653
Tim Eapen <tim.eapen@gmail.com>
2017-05-14 20:02:04 -0400

    TE - deletes

--- ruby/pretty_json.rb
+++
@@ -1,7 +0,0 @@
-x = {"hello" => "world", this: {"apple" => 4, tastes: "delicious"}}
-
-require 'json'
-puts x.to_json
-# {"hello":"world","this":{"apple":4,"tastes":"delicious"}}
-
-puts JSON.pretty_generate( x )
--- ruby/pretty_yaml.rb
+++
@@ -1,4 +0,0 @@
-x = {"hello" => "world", this: {"apple" => 4, tastes: "delicious"}}
-
-require 'yaml'
-puts x.to_yaml
